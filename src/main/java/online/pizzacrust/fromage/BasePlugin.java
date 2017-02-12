231e59b93e713cb5456ec011fb77c4ae3dbecbad
PizzaCrust <pizzacrustcoder@gmail.com>
2017-02-12 16:10:37 +0700

    Fromage can now start in Sponge

---
+++ src/main/java/online/pizzacrust/fromage/BasePlugin.java
@@ -0,0 +1,45 @@
+package online.pizzacrust.fromage;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+import online.pizzacrust.fromage.api.Fromage;
+import online.pizzacrust.fromage.api.LuaPlugin;
+import online.pizzacrust.fromage.common.LuaObject;
+import online.pizzacrust.fromage.common.env.LuaEnvironment;
+
+/**
+ * Represents a base to all implementations of {@link Fromage}.
+ *
+ * @since 1.0-SNAPSHOT
+ * @author PizzaCrust
+ */
+public abstract class BasePlugin {
+
+    /**
+     * Retrieves the plugins dir of lua plugins.
+     * @return
+     */
+    abstract File getLuaPluginsDir();
+
+    /**
+     * Represents a common implementation of the start process of a plugin.
+     */
+    protected void pluginEnabled() {
+        LuaEnvironment luaEnvironment = new LuaEnvironment();
+        luaEnvironment.initScope();
+        File[] luaFiles = getLuaPluginsDir().listFiles((dir, name) -> name.endsWith(".lua"));
+        for (File luaFile : luaFiles != null ? luaFiles : new File[0]) {
+            LuaObject.GLOBAL_SCOPE.loadfile(luaFile.getAbsolutePath());
+        }
+        Fromage.PLUGINS.forEach(LuaPlugin::enable);
+    }
+
+    /**
+     * Represents a common implementation of the end process of a plugin.
+     */
+    protected void pluginDisabled() {
+        Fromage.PLUGINS.forEach(LuaPlugin::disable);
+    }
+
+}
--- src/main/java/online/pizzacrust/fromage/FromageSpongePlugin.java
+++ src/main/java/online/pizzacrust/fromage/FromageSpongePlugin.java
@@ -1,7 +1,38 @@
 package online.pizzacrust.fromage;
 
+import com.google.inject.Inject;
+
+import org.slf4j.Logger;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameStartingServerEvent;
+import org.spongepowered.api.event.game.state.GameStoppingServerEvent;
 import org.spongepowered.api.plugin.Plugin;
 
+import java.io.File;
+
 @Plugin(id = "fromage")
-public class FromageSpongePlugin {
+public class FromageSpongePlugin extends BasePlugin {
+
+    @Inject
+    private Logger logger;
+
+    @Override
+    File getLuaPluginsDir() {
+        return Sponge.getGame().getConfigManager().getPluginConfig(this).getDirectory().toFile();
+    }
+
+    @Listener
+    public void onServerStart(GameStartingServerEvent e) {
+        logger.info("Initialising Fromage...");
+        logger.info("Using {} as a directory for loading Lua plugins.", getLuaPluginsDir().getPath());
+        pluginEnabled();
+    }
+
+    @Listener
+    public void onServerEnd(GameStoppingServerEvent e) {
+        logger.info("Ending Fromage...");
+        pluginDisabled();
+    }
+
 }
