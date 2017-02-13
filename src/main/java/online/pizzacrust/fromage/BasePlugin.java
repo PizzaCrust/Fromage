d13cf22517746056677272c05bdaaba3d8c3edef
PizzaCrust <pizzacrustcoder@gmail.com>
2017-02-13 23:37:19 +0700

    Don't shade, automatically download libraries and add them to SystemClassLoader.

--- src/main/java/online/pizzacrust/fromage/BasePlugin.java
+++ src/main/java/online/pizzacrust/fromage/BasePlugin.java
@@ -3,12 +3,16 @@ package online.pizzacrust.fromage;
 import org.luaj.vm2.LuaValue;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FilenameFilter;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 
 import online.pizzacrust.fromage.api.Fromage;
 import online.pizzacrust.fromage.api.LuaPlugin;
@@ -36,7 +40,11 @@ public abstract class BasePlugin {
      */
     abstract CommonLogger getAbstractLogger();
 
-    void addURL(URL url) {
+    private File getLibrariesDir() {
+        return new File(getLuaPluginsDir(), "libraries");
+    }
+
+    private void addURL(URL url) {
         URLClassLoader urlClassLoader = (URLClassLoader) Thread.currentThread()
                 .getContextClassLoader();
         Class<?> currentLoader = Thread.currentThread().getContextClassLoader().getClass();
@@ -50,10 +58,63 @@ public abstract class BasePlugin {
         }
     }
 
+    private void download(URL url, File file) throws IOException {
+        ReadableByteChannel byteChannel = Channels.newChannel(url.openStream());
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+        fileOutputStream.getChannel().transferFrom(byteChannel, 0, Long.MAX_VALUE);
+    }
+
     /**
      * Represents a common implementation of the start process of a plugin.
      */
     void pluginEnabled() {
+        getAbstractLogger().info("Creating/retrieving plugins directory...");
+        getLuaPluginsDir().mkdirs();
+        getAbstractLogger().info("Creating/retrieving libraries directory...");
+        if (!getLibrariesDir().exists()) {
+            getLibrariesDir().mkdirs();
+            try {
+                URL reflections = new URL("http://central.maven" +
+                        ".org/maven2/org/reflections/reflections/0.9.10/reflections-0.9.10.jar");
+                URL javaassist = new URL("http://central.maven" +
+                        ".org/maven2/org/javassist/javassist/3.18.2-GA/javassist-3.18.2-GA.jar");
+                URL luaj = new URL("http://central.maven" +
+                        ".org/maven2/org/luaj/luaj-jse/3.0.1/luaj-jse-3.0.1.jar");
+                File reflectionsJar = new File(getLibrariesDir(), "reflections-lib.jar");
+                File javaassistJar = new File(getLibrariesDir(), "javaassist-lib.jar");
+                File luajJar = new File(getLibrariesDir(),"luaj.jar");
+                getAbstractLogger().info("Downloading reflections...");
+                download(reflections, reflectionsJar);
+                getAbstractLogger().info("Downloading javaassist...");
+                download(javaassist, javaassistJar);
+                getAbstractLogger().info("Downloading luaj...");
+                download(luaj, luajJar);
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(-1);
+            }
+            getAbstractLogger().info("Loading libraries...");
+            File[] libraryFiles = getLibrariesDir().listFiles((dir, name) -> name.endsWith(".jar"));
+            for (File library : libraryFiles != null ? libraryFiles : new File[0]) {
+                try {
+                    addURL(library.toURI().toURL());
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                    System.exit(-1);
+                }
+            }
+        } else {
+            getAbstractLogger().info("Loading libraries...");
+            File[] libraryFiles = getLibrariesDir().listFiles((dir, name) -> name.endsWith(".jar"));
+            for (File library : libraryFiles != null ? libraryFiles : new File[0]) {
+                try {
+                    addURL(library.toURI().toURL());
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                    System.exit(-1);
+                }
+            }
+        }
         getAbstractLogger().info("Current classloader: {}", Thread.currentThread()
                 .getContextClassLoader().getClass().getName());
         if (!((Thread.currentThread().getContextClassLoader() instanceof URLClassLoader))) {
@@ -78,8 +139,6 @@ public abstract class BasePlugin {
         LuaEnvironment luaEnvironment = new LuaEnvironment();
         getAbstractLogger().info("Initialising the environment's scope...");
         luaEnvironment.initScope();
-        getAbstractLogger().info("Creating/retrieving plugins directory...");
-        getLuaPluginsDir().mkdirs();
         getAbstractLogger().info("Finding lua plugin files in the plugins directory...");
         File[] luaFiles = getLuaPluginsDir().listFiles((dir, name) -> name.endsWith(".lua"));
         getAbstractLogger().info("Found {} lua plugin files in the plugins directory!", String
